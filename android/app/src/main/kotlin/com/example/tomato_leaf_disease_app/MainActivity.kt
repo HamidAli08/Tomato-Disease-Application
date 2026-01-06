cf683497d281affb67a2f9974e41551eff3cdcbf
HamidAli08 <hamidaliha007@gmail.com>
2026-01-07 02:21:07 +0500

    initial Commit

---
+++ android/app/src/main/kotlin/com/example/tomato_leaf_disease_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tomato_leaf_disease_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF2E7D32);
+  static const secondary = Color(0xFF66BB6A);
+  static const background = Color(0xFFF1F8E9);
+  static const textDark = Color(0xFF1B5E20);
+}
